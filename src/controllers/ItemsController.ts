84180bfd7c1e76250b32ea2ec7d61c645b2c0582
João Eliandro <eliandrogermano@gmail.com>
2020-06-04 04:32:40 -0300

    Add the class to control the functions

---
+++ src/controllers/ItemsController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import knex from 'knex';
+
+class ItemsController {
+
+    async index(request: Request, response: Response) {
+        try {
+
+            const items = await knex('items').select('*');
+
+            const serializedItems = items.map(item => {
+                return {
+                    id: item.id,
+                    title: item.title,
+                    image_url: `http://localhost:3333/uploads/${item.image}`,
+                };
+            })
+
+            return response.json(serializedItems);
+        } catch (error) {
+            return response.status(400).json({ error });
+
+        }
+    }
+}
+
+export default ItemsController;
