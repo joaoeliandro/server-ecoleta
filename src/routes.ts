cf2b9b5cea7efe9a56298142c4896ea54451f009
João Eliandro <eliandrogermano@gmail.com>
2020-06-04 04:32:11 -0300

    Abstract function routes to the controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,26 @@
 import express from 'express';
 
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: "Hello World!" });
-});
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+/**
+ * Padrão de nomes das funções quando usar Controller: 
+ * index (listagem),
+ * show (mostrar unico registro),
+ * create (criar),
+ * update (atualizar),
+ * delete (deletar)
+ * */
+ 
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
