a8d9c4608e7fd9be7ac7f11af0f1ac78876c19f0
João Eliandro <eliandrogermano@gmail.com>
2020-06-06 21:37:29 -0300

    Add serialized and config for image_url to mobile app

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -9,6 +9,7 @@ class PointsCotroller {
         const parsedItems = String(items)
             .split(',')
             .map(item => Number(item.trim()));
+
         try {
 
             const points = await knex('points')
@@ -19,7 +20,14 @@ class PointsCotroller {
                 .distinct()
                 .select('points.*');
 
-            return response.json(points);
+            const serializedPoints = points.map(point => {
+                return {
+                    ...point,
+                    image_url: `http://192.168.0.110:3333/uploads/${point.image}`,
+                };
+            });
+
+            return response.json(serializedPoints);
         } catch (error) {
             return response.status(400).json({ error });
         }
@@ -35,11 +43,16 @@ class PointsCotroller {
             return response.status(400).json({ message: 'Point not found!' });
         }
 
+        const serializedPoint = {
+            ...point,
+            image_url: `http://192.168.0.110:3333/uploads/${point.image}`,
+        };
+
         const items = await knex('items')
             .join('point_item', 'items.id', '=', 'point_item.item_id')
             .where('point_item.point_id', id).select('items.title');
 
-        return response.json({ point, items });
+        return response.json({ serializedPoint, items });
     }
 
     async create(request: Request, response: Response) {
@@ -55,7 +68,7 @@ class PointsCotroller {
         } = request.body;
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -73,12 +86,15 @@ class PointsCotroller {
 
             const point_id = insertedIds[0];
 
-            const pointItems = items.map((item_id: number) => {
-                return {
-                    item_id,
-                    point_id,
-                }
-            });
+            const pointItems = items
+                .split(',')
+                .map((item: string) => Number(item.trim()))
+                .map((item_id: number) => {
+                    return {
+                        item_id,
+                        point_id,
+                    }
+                });
 
             await trx('point_item').insert(pointItems);
 
