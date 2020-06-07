3ba8ac2921351abd611f4f375c5c04e2e6a5318b
João Eliandro <eliandrogermano@gmail.com>
2020-06-06 21:38:19 -0300

    Add the multer config for image uploads

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 
+import multer from 'multer'
+import multerConfig from './config/multer';
+
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
+
 
 /**
  * Padrão de nomes das funções quando usar Controller: 
@@ -16,10 +21,10 @@ const itemsController = new ItemsController();
  * update (atualizar),
  * delete (deletar)
  * */
- 
+
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create);
+routes.post('/points', upload.single('image'), pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
